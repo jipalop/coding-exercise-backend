7e388348a58dc4bd05ce92fbe0d42f1a6f0760da
jipalop <jose.ignacio.palop@gmail.com>
2018-03-18 18:43:45 +0100

    search recipe service

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -13,10 +23,56 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ApiController extends Controller
 {
-
     /**
-     * @Route("/login_check", name="login_check")
+     * @Rest\Get("/v1/recipe/{search}.{_format}", name="recipe_list", defaults={"_format":"json"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Gets recipes based on passed search parameter."
+     * )
+     *
+     *
+     * @SWG\Parameter(
+     *     name="search",
+     *     in="path",
+     *     type="string",
+     *     description="The search string"
+     * )
+     *
+     *
+     * @SWG\Tag(name="Recipe")
+     * @param Request $request
+     * @param $search
+     * @return Response
      */
-    public function loginCheckAction() {}
+    public function getRecipeAction(Request $request, $search)
+    {
+        $serializer = $this->get('jms_serializer');
+        $client = new Client();
+        $code = 200;
+        $error = false;
+        $message = '';
+        $dataJson = '';
+        $dataArray = array();
 
+        try {
+            $uri = 'http://www.recipepuppy.com/api/?q=' . $search;
+            $res = $client->request('GET', $uri);
+            $dataJson = $res->getBody()->getContents();
+            $dataArray = json_decode($dataJson, true);
+
+        } catch (Exception $ex) {
+            $code = 500;
+            $error = true;
+            $message = "An error has occurred trying to get the current Board - Error: {$ex->getMessage()}";
+        }
+
+        $response = [
+            'code' => $code,
+            'error' => $error,
+            'data' => $code == 200 ? $dataArray['results'] : $message,
+        ];
+
+        return new Response($serializer->serialize($response, 'json'));
+    }
 }
